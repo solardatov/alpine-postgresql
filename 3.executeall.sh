04430231e584306de47d73785fc71638662e11bd
Ioseph Kim <iosephkim@gmail.com>
2019-07-05 16:17:11 +0000

    change for current version

--- 3.executeall.sh
+++ 3.executeall.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 
-for i in `docker ps -a | awk '{print $NF}' | grep postgresql- | sort`
+for i in `docker ps -a | awk '{print $NF}' | grep postgresql- | sort -t '-' -k 2 -n`
 do
 	echo "[$i]"
 	docker exec -it -e PGUSER=postgres -e PGDATABASE=template1 $i "$@"
