fd46c81f21371c5ff982a4873661946e9598f2cc
Ioseph Kim <iosephkim@gmail.com>
2019-07-06 11:55:12 +0000

    add ansi color

--- 3.executeall.sh
+++ 3.executeall.sh
@@ -3,7 +3,7 @@
 
 for i in `docker ps -a | awk '{print $NF}' | grep postgresql- | sort -t '-' -k 2 -n`
 do
-	echo "[$i]"
+	echo -e "\033[01;34m[$i]\033[00m"
 	docker exec -it -e PGUSER=postgres -e PGDATABASE=template1 $i "$@"
 	echo "================================================================================"
 done
